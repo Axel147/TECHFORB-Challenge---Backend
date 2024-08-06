9447b52db89b047831b74984f43e12e139e7d3c4
Axel147 <lopez.b.axel@gmail.com>
2024-08-05 22:42:12 -0300

    se agrego cors a las dependencias

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import routes from "./src/routes/index.routes";
 import bodyParser from 'body-parser';
 import { PORT } from "./src/config/config";
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
