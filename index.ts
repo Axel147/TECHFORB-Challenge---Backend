7fc534475aff224e6160ce388b37553a4960c148
Axel147 <lopez.b.axel@gmail.com>
2024-08-06 01:34:09 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 
-const port = parseInt(PORT + "") //solo para ver si render puede escuchar este puerto
+const port = 3000 //solo para ver si render puede escuchar este puerto
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Servidor ejecutandose en http://localhost:${port || 3001}/`);
