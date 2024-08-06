d5b38e9714aed10203a3e738b5a2624fd5360263
Axel147 <lopez.b.axel@gmail.com>
2024-08-06 17:42:56 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ const cors = require('cors');
 
 const app = express();
 
-//app.use(cors);
+//app.use(express.urlencoded({extended: true}))
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
